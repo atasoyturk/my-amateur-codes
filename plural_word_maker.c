2b9927600191439a7ad8dfc288726d7cd3ff069a
Ata Soytürk <atasoyturkkk@hotmail.com>
2025-04-11 20:28:09 +0300

    Update and rename str_pluralmaker.c to plural_word_maker.c

--- plural_word_maker.c
+++ plural_word_maker.c
@@ -36,19 +36,19 @@ void plural(char* s1, char* s2){
     else if (length > 1 && ((s1[length - 1] == 's') || (s1[length - 1] == 'h' && s1[length - 2] == 's') ||
             (s1[length - 1] == 'h' && s1[length - 2] == 'c') || (s1[length - 1] == 's' && s1[length - 2] == 'c')))
             {
-                for (i = 0; i < length; i++){
+            for (i = 0; i < length; i++){
                 s2[i] = s1[i];
-                }
+            }
                 
-                s2[i++] = 'e';
-                s2[i++] = 's';
-                s2[i] = '\0';
+            s2[i++] = 'e';
+            s2[i++] = 's';
+            s2[i] = '\0';
             }else {
                 for (i = 0; i < length; i++){
-                s2[i] = s1[i];
+                    s2[i] = s1[i];
                 }
                 s2[i++] = 's';
-            s2[i] = '\0';
+                s2[i] = '\0';
             }
 }
 
